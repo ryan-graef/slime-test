6cb8756704fb3e7bf8123f7feecc738a1e93e1b3
Ryan Graef <cudascubby@gmail.com>
2016-04-13 17:34:43 -0500

    collisions

--- src/state/MainState.js
+++ src/state/MainState.js
@@ -1,10 +1,12 @@
 MainState = function(){ }
 
 MainState.prototype = {
-    slimePointCount: 25,
+    slimePointCount: 50,
     slimePoints: [],
     slimeSprings: [],
     springLineBmd: null,
+    slimeCollisionGroup: null,
+    worldCollisionGroup: null,
     slimeCenterPoint: null,
     cursorKeys: null,
 
@@ -16,6 +18,10 @@ MainState.prototype = {
         game.physics.startSystem(Phaser.Physics.P2JS);
         game.physics.p2.gravity.y = 100;
 
+        this.slimeCollisionGroup = game.physics.p2.createCollisionGroup();
+        this.worldCollisionGroup = game.physics.p2.createCollisionGroup();
+        game.physics.p2.updateBoundsCollisionGroup();
+
         this.cursorKeys = game.input.keyboard.createCursorKeys();
 
         var bmd = game.add.bitmapData(6, 6);
@@ -23,6 +29,21 @@ MainState.prototype = {
         bmd.context.fillRect(0, 0, 6, 6);
         bmd.dirty = true;
         game.cache.addBitmapData('dot', bmd);
+
+        var bmd2 = game.add.bitmapData(640, 32);
+        bmd2.context.fillStyle = '#FFFFFF';
+        bmd2.context.fillRect(0, 0, bmd2.width, bmd2.height);
+        bmd2.dirty = true;
+        game.cache.addBitmapData('rect', bmd2);
+
+        var platform = game.add.sprite(0, 0, bmd2);
+        platform.anchor.setTo(0, 0);
+        platform.x = game.world.centerX-bmd2.width/2
+        platform.y = game.world.centerY-bmd2.height/2
+        game.physics.p2.enable(platform);
+        platform.body.setCollisionGroup(this.worldCollisionGroup);
+        platform.body.collides([this.slimeCollisionGroup], null, true);
+        platform.body.dynamic = false;
 
         this.springLineBmd = game.add.bitmapData(game.width, game.height);
         this.springLineBmd.context.strokeStyle = "#FFFFFF";
@@ -43,17 +64,13 @@ MainState.prototype = {
         this.springLineBmd.dirty = true;
 
 
-        this.slimePoints.forEach(function(point){
-            if(this.cursorKeys.up.isDown){
-                point.body.velocity.y = -200;
-            }else if(this.cursorKeys.left.isDown){
-                point.body.velocity.x += -10;
-            }else if(this.cursorKeys.right.isDown){
-                point.body.velocity.x += 10;
-            }
-
-
-        }, this);
+        if(this.cursorKeys.up.isDown){
+            this.slimeCenterPoint.body.velocity.y = -200;
+        }else if(this.cursorKeys.left.isDown){
+            this.slimeCenterPoint.body.velocity.x += -50;
+        }else if(this.cursorKeys.right.isDown){
+            this.slimeCenterPoint.body.velocity.x += 50;
+        }
     },
 
     render: function(){
@@ -62,12 +79,12 @@ MainState.prototype = {
 
     createSlime: function(){
         var centerX = game.world.randomX;
-        var centerY = game.world.centerY;
-        var averageDistanceFromCenter = 50;
+        var centerY = 100;
+        var averageDistanceFromCenter = 100;
         var spacing = Math.PI*2/this.slimePointCount;
         var surfaceArea = Math.pow(averageDistanceFromCenter, 2)*Math.PI;
-        var elacticity = 50;
-        var friction = 5;
+        var elacticity = 20;
+        var friction = 3;
 
 
 
@@ -77,6 +94,8 @@ MainState.prototype = {
 
             var point = game.add.sprite(pointX, pointY, game.cache.getBitmapData('dot'));
             game.physics.p2.enable(point);
+            point.body.setCollisionGroup(this.slimeCollisionGroup);
+            point.body.collides([this.worldCollisionGroup]);
             point.body.collideWorldBounds = true;
 
             this.slimePoints.push(point);
@@ -129,7 +148,7 @@ MainState.prototype = {
                     var them = connectTo[q];
                     var d = Math.sqrt(Math.pow(me.x - them.x, 2) + Math.pow(me.y - them.y, 2), 2);
 
-                    var spring = game.physics.p2.createSpring(me, them, d, elacticity, friction);
+                    var spring = game.physics.p2.createSpring(me, them, d, elacticity*4, friction);
                     this.slimeSprings.push(spring);
             }
 
@@ -137,6 +156,8 @@ MainState.prototype = {
            
             var centerSpring = game.physics.p2.createSpring(this.slimePoints[i], this.slimeCenterPoint, averageDistanceFromCenter, elacticity, friction)
             this.slimeSprings.push(spring);
+            this.slimeSprings.push(centerSpring);
+
         }
     }
 }
